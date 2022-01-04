ba5701c7a8adcea184a163a9f029a4b0f177d93b
Zachary Joyner <burmydread@protonmail.ch>
2022-01-04 17:00:33 -0500

    general cleanup

--- templates/get_stage_test.sh
+++ templates/get_stage_test.sh
@@ -4,5 +4,5 @@ latest="http://distfiles.gentoo.org/releases/amd64/autobuilds/latest-stage3.txt"
 stage3_path=$(curl $latest |  tail -n +3 | grep stage3-amd64 | cut -f 1,1 -d ' ' | grep {{ type }})
 echo {{ mirror }}/releases/amd64/autobuilds/$stage3_path > url.txt
 wget  {{ mirror }}/releases/amd64/autobuilds/$stage3_path -O \
-      stage3.tar.xz
-tar tf stage3.tar.xz
+      stage3.tbz2
+tar tf stage3.tbz2
